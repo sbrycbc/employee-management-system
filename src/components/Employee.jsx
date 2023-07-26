92591188ae44f61ba9f5df19b5ed849073b58fcb
sbrycbc <sabriyecbc@gmail.com>
2023-07-26 13:28:30 +0200

    added employeeList with useState..

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 
 
 
-const Employee = () => {
 
-  return (
-    <ul>
-        <li>Employee-1</li>
-        <li>Employee-2</li>
-        <li>Employee-3</li>
-    </ul>
-  )
+const Employee = ({employees}) => {
+
+    return (
+
+        <>
+        {
+            employees.map((employee) => (
+            <tr key={employee.id}>
+                <td>{employee.name}</td>
+                <td>{employee.email}</td>
+                <td>{employee.address}</td>
+                <td>{employee.phone}</td>
+                <td>
+                    <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                    <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                </td>
+            </tr>
+            ))
+        }
+         </>
+
+    )
 }
 
 export default Employee;
