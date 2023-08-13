c9ad4ac84eef320c167f9749c40536fde7f04252
sbrycbc <sabriyecbc@gmail.com>
2023-08-13 22:50:05 +0200

    updated name order with localeCompare()

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -52,7 +52,7 @@ const EmployeeList = () => {
           </thead>
           <tbody>
           {
-                    employees.map((employee) =>(
+                    employees.sort((a,b) => a.name.localeCompare(b.name)).map((employee) =>(
                         <tr key={employee.id}>
                             <Employee employee={employee} />
                         </tr>
