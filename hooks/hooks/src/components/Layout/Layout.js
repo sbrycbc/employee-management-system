e49882fbebab4cd951e43f7aae0fc02e6bb0c285
sbrycbc <sabriyecbc@gmail.com>
2023-08-15 16:00:52 +0200

    initial commit...

---
+++ hooks/hooks/src/components/Layout/Layout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import UseRef from "../UseRef/useRef.jsx";
+import UseReducer from "../UseReducer/useReducer.jsx";
+
+
+
+const Layout = () => {
+    return (
+        <div>
+
+            <div>
+              <UseRef />
+            </div>
+
+            <div>
+               <UseReducer />
+            </div>
+            
+        </div>
+    )
+}
+
+
+export default Layout;
+
+
+
